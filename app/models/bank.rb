325d171531cfcfe8c0fb30093de83d6390bdb1a1
t-koma <last3121@gmail.com>
2019-12-26 02:19:47 +0000

    微調整

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,11 +1,6 @@
 class Bank < ApplicationRecord
 	validates :money, presence: true
 	validates :money, numericality:{greater_than: 0} 
-	validates :bank_day,presence:true
-
-	def get_bank
-
-	end
-		
+	validates :bank_day,presence:true	
 	
 end
